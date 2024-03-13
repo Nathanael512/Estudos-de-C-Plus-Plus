3d554064ff928b2790029ae5dbc7bf708f128e07
NathanaelDEV93 <natanaeldev93@gmail.com>
2024-03-13 13:50:38 -0300

    Variáveis Múltiplas

--- multiplas_variaveis.cc
+++ multiplas_variaveis.cc
@@ -1,11 +1,40 @@
 #include <iostream>
 using namespace std;
 
+#define pi 3.1415
+#define perfil cout << "NatanaelDEV93\n\n";
+
+
 int main(){
 
+   int life=100, ammo=60, lifes=5;
 
+   cout << pi << "\n\n"; // print ao valor da constância.
+
+   perfil // Não precisa usar o cout, porque ele já foi declarado na constância.
 
     return 0;
     
 
 }
+
+
+/* -- Declarações de variáveis --
+
+Variáveis separadas
+
+int life;
+int ammo;
+int lifes;
+
+Variáveis múltiplas
+
+int life, ammo, lifes;
+
+Variáveis 'múltiplas' com declaração de tipos (Cada variável é declarada com seu tipo)
+
+int life, int ammo, int lifes;
+
+
+
+*/
